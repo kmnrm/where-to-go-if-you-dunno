a26249287c4c436a13a5d35e18f83d840b8112f5
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-06-04 14:56:32 +0800

    Add blank start page

--- afisha/urls.py
+++ afisha/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from afisha import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.start_page)
 ]
---
+++ afisha/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'start_page.html')
