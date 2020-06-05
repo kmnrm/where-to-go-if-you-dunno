085f5376e3da6358ca6f378a7a111be37e7cc965
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-06-05 22:43:59 +0800

    Get locations from database

--- afisha/views.py
+++ afisha/views.py
@@ -1,7 +1,40 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from places.models import Place
+
+
+def make_feature_for_geojson(place):
+	feature = {
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [place.longitude, place.latitude],
+		},
+		"properties": {
+			"title": place.title,
+			"placeId": place.pk,
+			"detailsUrl": "static/places/roofs24.json",
+		}
+	}
+	return feature
 
 
 def index(request):
-    return render(request, 'index.html')
+	places = Place.objects.all()
+
+	places_geojson = {
+		"type": "FeatureCollection",
+		"features": [],
+	}
+
+	for place in places:
+		print(place.title)
+		feature = make_feature_for_geojson(place)
+		places_geojson["features"].append(feature)
+
+	context = {
+		"places_geojson": places_geojson,
+	}
+
+	return render(request, 'index.html', context)
