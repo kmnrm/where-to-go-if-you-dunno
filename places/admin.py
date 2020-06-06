23ae2b52856b8804d67c24e8b9b0edac3aebea42
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-06-06 20:14:55 +0800

    Show images previews in admin

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Place, PlaceImage
 
 
 class PlaceImageInline(admin.TabularInline):
 	model = PlaceImage
+	fields = ['image', 'get_preview', 'order_number']
+	readonly_fields = ['get_preview']
+
+	def get_preview(self, place):
+		return mark_safe('<img src="{url}" height="200" />'.format(url=place.image.url))
 
 
 @admin.register(Place)
