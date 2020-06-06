f304ce5aa807eadefb387d2aa05291daa3e86081
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-06-06 20:37:04 +0800

    Avoid simultaneous using of tab and space

--- places/models.py
+++ places/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200)
-    description_short = models.TextField('Краткое описание', blank=True)
-    description_long = models.TextField('Подробное описание', blank=True)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+	title = models.CharField(max_length=200)
+	description_short = models.TextField('Краткое описание', blank=True)
+	description_long = models.TextField('Подробное описание', blank=True)
+	latitude = models.FloatField()
+	longitude = models.FloatField()
 
-    def __str__(self):
-        return f"{self.title}"
+	def __str__(self):
+		return f"{self.title}"
 
 
 class PlaceImage(models.Model):
-    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='images/')
-    order_number = models.IntegerField()
+	place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
+	image = models.ImageField(upload_to='images/')
+	order_number = models.IntegerField(default=2)
 
-    def __str__(self):
-        return f"{self.order_number} {self.place.title}"
+	def __str__(self):
+		return f"{self.order_number} {self.place.title}"
