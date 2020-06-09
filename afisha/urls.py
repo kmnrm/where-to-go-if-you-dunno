5c5475597e898ae291098d6fe3d049dfb0391318
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-06-10 00:38:08 +0800

    Correct views importing mistake

--- afisha/urls.py
+++ afisha/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from afisha import views
+from places import views
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
